1e27550a00066d8971ea940d719c79badb5b9115
Juliss4 <karenjulissa19999@gmail.com>
2020-02-08 18:04:50 -0600

    agregando frase motivadora

--- RegistroN/RegistroN/Form1.cs
+++ RegistroN/RegistroN/Form1.cs
@@ -19,7 +19,7 @@ namespace RegistroN
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Tener grandes expectativas es la clave de todo");
         }
     }
 }
